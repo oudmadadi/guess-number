a10b30926fb8c28a6742381cd34c3f82bb7ae304
oud <oudmadadi@gmail.com>
2025-08-10 12:16:33 +0330

    Third Commit

--- hads adad.py
+++ hads adad.py
@@ -1,16 +1,19 @@
 from random import randint
 
 javab = randint(1, 6)
+count = 0
 
 while True:
     i = int(input("Guess what number is chosen? "))
+    count += 1
 
     if i > javab:
         print("My number is smaller!")
     elif i < javab:
         print("My number is bigger!")
     else:
-        print(f"Your guess is correct. That was {javab}")
+        print(f"Your guess is correct. That was {javab}.")
+        print (f"You guessed {count} times!")
         break
 
         
